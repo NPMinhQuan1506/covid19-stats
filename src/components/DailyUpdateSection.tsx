f773f1cb01a1a3dcae3c845f295bdf7f39f039c5
Quang Pham <qquang24t5@gmail.com>
2022-03-28 17:09:42 +0700

    Chỉnh màu ở DailyUpdateSection cho giống VnExpress

--- src/components/DailyUpdateSection.tsx
+++ src/components/DailyUpdateSection.tsx
@@ -22,16 +22,18 @@ export default function DailyUpdateSection() {
       <FlexBox justifyContent="center">
         <FlexBox direction="column" alignItems="center">
           <Typography variant="body1">Ca mới</Typography>
-          <Box bgcolor="warning.main" padding={2}>
-            <Typography variant="h6">
-              {summary === null ? "Đang tải..." : summary.Global.NewConfirmed}
+          <Box bgcolor="#f7f7f7" padding={2}>
+            <Typography variant="h6" color="#ff0000">
+              {summary === null
+                ? "Đang tải..."
+                : `+ ${summary.Global.NewConfirmed}`}
             </Typography>
           </Box>
         </FlexBox>
         <FlexBox direction="column" alignItems="center">
           <Typography variant="body1">Tổng</Typography>
-          <Box bgcolor="error.main" padding={2}>
-            <Typography variant="h6">
+          <Box bgcolor="#f3585b" padding={2}>
+            <Typography variant="h6" color="#fff">
               {summary === null ? "Đang tải..." : summary.Global.TotalConfirmed}
             </Typography>
           </Box>
