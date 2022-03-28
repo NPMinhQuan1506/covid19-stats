35ed8d4002129c3217d38d034b4ac6a06eb83d0f
Quang Pham <qquang24t5@gmail.com>
2022-03-28 11:47:31 +0700

    Cài và setup ReactRouter; Thêm HomePage, AboutPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AboutPage, HomePage } from "./pages";
 
 function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
