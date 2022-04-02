9f9ee67aabaa13a4532ab8ed7b92cbfd338514a7
minhnguyencong4 <74465589+minhnguyencong4@users.noreply.github.com>
2022-04-02 13:07:22 +0700

    Create DailyOverview.tsx

---
+++ src/components/DailyOverview.tsx
@@ -0,0 +1,40 @@
+import { Container, Typography } from "@mui/material";
+import { color } from "@mui/system";
+import { AxiosResponse } from "axios";
+import { useState, useEffect } from "react";
+import { Summary } from "../api-types";
+import Request from "../Request"
+
+      
+
+
+export default function DailyOVerview() {
+    const [summary, setSummary] = useState<Summary | null>(null);
+
+    useEffect(() => {
+    Request.get("/summary").then((response: AxiosResponse<Summary>) =>
+      setSummary(response.data)
+    );
+  }, []);
+
+// trước khi vẽ giao diện, useEffect run code trc khi đẩy lên giao diện html (hàm tự khai báo)
+
+   
+   return <Container>
+        <Typography>
+
+        Ngày hôm qua, Thế giới đã ghi nhận <b >{summary === null ? "Loading..." : summary.Global.NewConfirmed}</b>  ca,
+
+        nâng tổng số ca nhiễm trên Thế Giới từ ngày 27/4 đến nay lên <b>{summary === null ? "Loading..." : summary.Global.TotalConfirmed}</b> ca. 
+        
+       <p>Cùng ngày, Bộ Y tế Thế Giới đã công bố <b>{summary === null ? "Loading..." : summary.Global.NewRecovered}</b> người khỏi bệnh
+
+         và <b>{summary === null ? "Loading..." : summary.Global.NewDeaths}</b> ca tử vong  </p> 
+
+
+        {/* ? và : giống if và else */}
+   
+        </Typography>
+    </Container>
+  
+}
