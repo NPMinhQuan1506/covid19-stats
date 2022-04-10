9a593c65e2938b3167b03f0fbb0e03e062c10d1b
Mou <npminhquan1506@gmail.com>
2022-04-10 22:35:48 +0700

    Update count up DailyUpdateSection

--- src/components/DailyUpdateSection.tsx
+++ src/components/DailyUpdateSection.tsx
@@ -4,6 +4,7 @@ import FlexBox from "./FlexBox";
 import Request from "../Request";
 import { Summary } from "../api-types";
 import { AxiosResponse } from "axios";
+import CountUp from 'react-countup';
 
 export default function DailyUpdateSection() {
   const [summary, setSummary] = useState<Summary | null>(null);
@@ -23,9 +24,7 @@ export default function DailyUpdateSection() {
           <Typography variant="body1">Ca mới</Typography>
           <Box bgcolor="#f7f7f7" padding={2}>
             <Typography variant="h6" color="#ff0000">
-              {summary === null
-                ? "Đang tải..."
-                : `+ ${(summary.Global.NewConfirmed).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`}
+              + {summary === null ? "Đang tải..." :  <CountUp end={summary.Global.NewConfirmed} separator='.' duration={2} />}
             </Typography>
           </Box>
         </FlexBox>
@@ -33,7 +32,7 @@ export default function DailyUpdateSection() {
           <Typography variant="body1">Tổng</Typography>
           <Box bgcolor="#f3585b" padding={2}>
             <Typography variant="h6" color="#fff">
-              {summary === null ? "Đang tải..." : (summary.Global.TotalConfirmed).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+              {summary === null ? "Đang tải..." :  <CountUp end={summary.Global.TotalConfirmed} separator='.' duration={2} />}
             </Typography>
           </Box>
         </FlexBox>
